3c0a2c3b34137b416cd8dc5a525b1388188fad2e
yoon22 <11yoon30@gmail.com>
2019-04-22 16:10:13 +0900

    자리이동

---
+++ src/main/java/com/osf/web/controller/Exam.java
@@ -0,0 +1,13 @@
+package com.osf.web.controller;
+
+public class Exam {
+	public String name;
+	
+	public static void main(String[] args) {
+		Exam e = new Exam();
+		if(e.name.equals("홍길동")) {
+			System.out.println("아무튼 홍길동임!");
+			
+		}
+	}
+}
